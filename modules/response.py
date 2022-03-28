70aefec57839f365caa2bb6ae1674223992ff0fd
Deadshot0x7 <sviquarahmed@gmail.com>
2022-03-28 12:26:47 +0530

    Added

--- modules/response.py
+++ modules/response.py
@@ -1,6 +1,8 @@
 import time  as t 
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 def personal(update, c):
+    update.message_text("Welcome to the Personal Function")
+
     return "Welcome to the Personal function"
 
 
